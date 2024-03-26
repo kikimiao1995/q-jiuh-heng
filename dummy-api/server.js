bc7ff4507d759444b6d6f380ba5d69ddf7310bd5
kikimiao <b02601049@ntu.edu.tw>
2024-03-26 10:59:38 +0800

    change to json-server@0.17.4

--- dummy-api/server.js
+++ dummy-api/server.js
@@ -1,11 +1,23 @@
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 5000;
+
+// server.use(middlewares);
+// server.use(router);
+// server.listen(port, () => {
+//   console.log("JSON Server is running");
+// });
+
+// server.js
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => {
+server.listen(3000, () => {
   console.log("JSON Server is running");
 });
